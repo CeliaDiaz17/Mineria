ff35717a8f9a3e133a9ee988538132a1700d89c9
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2023-10-24 17:03:33 +0200

    connect fixed

--- connect.py
+++ connect.py
@@ -1,20 +1,22 @@
-import cx_Oracle
+import getpass
+import os
+import traceback
+import oracledb
 
-# Configura la cadena de conexión
-connection = cx_Oracle.connect(
-    user="USER_W",
-    password="3VypJ8EZeVi4HNy",
-    dsn="dbmm_high"  # Debe coincidir con el alias en tnsnames.ora
-)
+un = 'admin'
+cs = '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g067633159c582f_dbmm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'
+pw = getpass.getpass(f'Enter password for {un}: ')
 
-# Crea un cursor
-cursor = connection.cursor()
+try:
 
-# Ejecuta consultas SQL
-cursor.execute("SELECT * FROM tu_tabla")
-for row in cursor.fetchall():
-    print(row)
+    connection = oracledb.connect(user=un, password=pw, dsn=cs)
 
-# Cierra el cursor y la conexión
-cursor.close()
-connection.close()
+    with connection.cursor() as cursor:
+        sql = """select systimestamp from dual"""
+        for r, in cursor.execute(sql):
+            print(r)
+
+except oracledb.Error as e:
+    error, = e.args
+    print(error.message)
+    traceback.print_tb(e.__traceback__)
