240a476c6f4cf0316c60c2e76b91e4d3e76b46fe
Guillermo Cubero Charco <Guillermo.Cubero@alu.uclm.es>
2023-11-08 17:03:53 +0100

    updated

--- connect.py
+++ connect.py
@@ -5,49 +5,55 @@ import oracledb
 import pandas as pd
 
 un = 'user_w'
-cs = '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g067633159c582f_dbmm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'
 pw = getpass.getpass(f'Enter password for {un}: ')
+schema_name = 'RAW_LAYER'  # Esquema de la base de datos
+
+cs = f'(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g067633159c582f_dbmm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes))(user={un}))(schema={schema_name})'
 
 try:
     connection = oracledb.connect(user=un, password=pw, dsn=cs)
+    cursor = connection.cursor()
 
-    with connection.cursor() as cursor:
-        # Crear una tabla "raw_data" para almacenar los datos brutos
-        create_table_sql = """
-        CREATE TABLE raw_data (
-            -- Columnas se definirán automáticamente desde el archivo CSV
-        )
-        """
-        cursor.execute(create_table_sql)
-        connection.commit()
+    # Ruta al directorio que contiene los archivos CSV
+    csv_directory = '/csv'
 
-        # Ruta al directorio que contiene los archivos CSV
-        csv_directory = '/csv'
+    for csv_file in os.listdir(csv_directory):
+        if csv_file.endswith('.csv'):
+            file_path = os.path.join(csv_directory, csv_file)
 
-        # Iterar a través de los archivos CSV y cargar los datos en la tabla
-        for csv_file in os.listdir(csv_directory):
-            if csv_file.endswith('.csv'):
-                file_path = os.path.join(csv_directory, csv_file)
+            # Utiliza pandas para leer las columnas del archivo CSV
+            df = pd.read_csv(file_path)
+            columns = df.columns.tolist()
 
-                # Utiliza pandas para leer las columnas del archivo CSV
-                df = pd.read_csv(file_path)
-                columns = df.columns.tolist()
+            # Extrae el nombre de la tabla del nombre del archivo CSV (sin la extensión)
+            table_name = os.path.splitext(csv_file)[0]
 
-                with open(file_path, 'r') as csv_file:
-                    next(csv_file)  # Saltar la primera fila si es una cabecera
-                    cursor.copyfrom(
-                        csv_file,
-                        'raw_data',
-                        columns=columns  # Utiliza las columnas leídas automáticamente
-                    )
+            # Crea una tabla para cada archivo CSV en el esquema específico
+            create_table_sql = f"""
+            CREATE TABLE {schema_name}.{table_name} (
+                {", ".join([f"{col} VARCHAR2(100)" for col in columns])} 
+                -- Define los tipos de datos adecuados
+            )
+            """
+            cursor.execute(create_table_sql)
 
-        connection.commit()
-        print("Datos cargados exitosamente en la tabla 'raw_data'.")
+            with open(file_path, 'r') as csv_file:
+                next(csv_file)  # Saltar la primera fila si es una cabecera
+                cursor.copyfrom(
+                    csv_file,
+                    f'{schema_name}.{table_name}',
+                    columns=columns
+                )
+
+    connection.commit()
+    print(f"Datos cargados en tablas individuales en el esquema {schema_name}.")
 
 except oracledb.Error as e:
     error, = e.args
     print(error.message)
-    traceback.print_tb(e.__traceback__)
+    traceback.print_tb(e.__traceback())
 
 finally:
+    cursor.close()
     connection.close()
+
