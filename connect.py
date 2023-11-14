4ba4517e118856815b805648e51a209ff7610015
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2023-11-14 09:48:31 +0100

    Scheme_silver created

--- connect.py
+++ connect.py
@@ -1,21 +1,22 @@
 import getpass
 import os
 import traceback
+import sys
 import oracledb
 import pandas as pd
 
-un = 'user_w'
+un = 'admin'
 pw = getpass.getpass(f'Enter password for {un}: ')
 schema_name = 'SCHEMA_SILVER'  # Reemplaza 'TUSCHEMA' con el nombre de tu esquema
 
-cs = f'(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g067633159c582f_dbmm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes))(user={un}))(schema={schema_name})'
+cs = f'(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g067633159c582f_dbmm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'
 
 try:
     connection = oracledb.connect(user=un, password=pw, dsn=cs)
     cursor = connection.cursor()
 
     # Ruta al directorio que contiene los archivos CSV
-    csv_directory = '/resultados'
+    csv_directory = 'resultados'
 
     for csv_file in os.listdir(csv_directory):
         if csv_file.endswith('.csv'):
@@ -28,44 +29,53 @@ try:
             # Extrae el nombre de la tabla del nombre del archivo CSV (sin la extensión)
             table_name = os.path.splitext(csv_file)[0]
 
-            # Mapeo de tipos de datos entre pandas y Oracle
-            oracle_data_types = {
-                'int64': 'NUMBER',   # Para enteros
-                'float64': 'FLOAT',  # Para números de punto flotante
-                # Para texto (ajusta la longitud según tus necesidades)
-                'object': 'VARCHAR2(255)'
-            }
+            # Verifica si la tabla ya existe
+            check_table_sql = f"SELECT COUNT(*) FROM ALL_TABLES WHERE TABLE_NAME = '{table_name}' AND OWNER = '{schema_name}'"
+            cursor.execute(check_table_sql)
+            existing_tables = cursor.fetchall()
 
-            # Crear una lista de definiciones de columna
-            column_definitions = [
-                f"{col} {oracle_data_types[df[col].dtype.name.upper()]}" for col in columns
-            ]
+            # Si la tabla no existe, créala
+            if not existing_tables:
+                # Mapeo de tipos de datos entre pandas y Oracle
+                oracle_data_types = {
+                    'int64': 'NUMBER',
+                    'float64': 'FLOAT',
+                    'object': 'VARCHAR2(255)'
+                }
 
-            # Crea una tabla para cada archivo CSV en el esquema específico
-            create_table_sql = f"""
-            CREATE TABLE {schema_name}.{table_name} (
-                {", ".join(column_definitions)}
-            )
-            """
-            cursor.execute(create_table_sql)
+                # Crear una lista de definiciones de columna
+                column_definitions = [
+                    f'"{col}" {oracle_data_types[df[col].dtype.name.lower()]}' for col in columns
+                ]
 
-            with open(file_path, 'r') as csv_file:
-                next(csv_file)  # Saltar la primera fila si es una cabecera
-                cursor.copyfrom(
-                    csv_file,
-                    f'{schema_name}.{table_name}',
-                    columns=columns
+                # Crea una tabla para cada archivo CSV en el esquema específico
+                create_table_sql = f"""
+                CREATE TABLE {schema_name}.{table_name} (
+                    {", ".join(column_definitions)}
                 )
+                """
+                cursor.execute(create_table_sql)
+
+                # Inserta datos desde el archivo CSV usando executemany
+                insert_sql = f"INSERT INTO {schema_name}.{table_name} VALUES ({', '.join([':' + col for col in columns])})"
+                data_to_insert = df.values.tolist()
+                cursor.executemany(insert_sql, data_to_insert)
+            else:
+                print(
+                    f"La tabla {table_name} ya existe en el esquema {schema_name}.")
 
     connection.commit()
     print(
         f"Datos cargados en tablas individuales en el esquema {schema_name}.")
 
+
 except oracledb.Error as e:
     error, = e.args
     print(error.message)
-    traceback.print_tb(e.__traceback())
+    traceback.print_tb(sys.exc_info()[2])
 
 finally:
-    cursor.close()
-    connection.close()
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals():
+        connection.close()
